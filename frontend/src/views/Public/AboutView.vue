e23143f507673a733d8a49ba9fb1fb7ef397c401
LankoDm <play585588@gmail.com>
2026-04-08 11:48:07 +0300

    Add text to the about us page + update docker-compose.yml

--- frontend/src/views/Public/AboutView.vue
+++ frontend/src/views/Public/AboutView.vue
@@ -3,14 +3,14 @@
     <div class="text-center mb-16">
       <h1 class="text-5xl font-black text-text mb-6">Хто ми такі?</h1>
       <p class="text-xl text-gray-500 font-medium max-w-2xl mx-auto">
-        Щось написати
+          OneaRoom - це команда фанатів своєї справи з Чернігова. Ми створюємо не просто кімнати із загадками, а цілі світи, в які ви можете зануритися з головою, не виїжджаючи з рідного міста.
       </p>
     </div>
     <div class="bg-secondary/20 p-8 md:p-12 rounded-3xl border border-secondary mb-20">
       <h2 class="text-3xl font-black text-text mb-8 text-center md:text-left">Наша місія</h2>
       <div class="flex flex-col md:flex-row gap-10 items-center md:items-start">
         <p class="text-gray-600 text-lg leading-relaxed font-medium md:w-1/2">
-          Тут також придумати
+            Ми віримо, що найкращий відпочинок - це зміна реальності. Наша мета - дати чернігівцям та гостям міста можливість стати головним героєм детективу, трилера або фантастичного фільму.
         </p>
         <ul class="space-y-4 font-bold text-gray-700 md:w-1/2">
           <li class="flex items-center gap-3">
