74b7e2dbcb6d6c7b468b9ae5cdf9662a61cb840b
LankoDm <play585588@gmail.com>
2026-04-21 18:50:57 +0300

    Add favicon, changes to the booking modal window

--- frontend/src/components/Room/RoomBookingModal.vue
+++ frontend/src/components/Room/RoomBookingModal.vue
@@ -163,7 +163,7 @@ onUnmounted(() => clearInterval(timerInterval.value));
                                    :class="validationErrors.guest_phone ? 'border-red-500' : 'border-secondary'">
                         </div>
                         <div>
-                            <label class="block text-sm font-bold text-gray-700 mb-1">Email</label>
+                            <label class="block text-sm font-bold text-gray-700 mb-1">Email (На цю пошту прийде квиток)</label>
                             <input v-model="bookingForm.email" type="email" class="w-full px-3 py-2 border rounded-xl"
                                    :class="validationErrors.guest_email ? 'border-red-500' : 'border-secondary'">
                         </div>
